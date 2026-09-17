9af6ee20e18e1f01b0a9064a4dc1cb2d18cdc3cd
Riya Kumari <riyaraju8412@gmail.com>
2026-09-17 19:33:28 +0530

    Add DFS approach for cycle detection in directed graph

--- src/dsa/datastructures/graphs/DetectCycleInDirectedGraph.java
+++ src/dsa/datastructures/graphs/DetectCycleInDirectedGraph.java
@@ -24,7 +24,7 @@ public class DetectCycleInDirectedGraph {
         else System.out.println("No, the given graph is not cyclic.");
     }
 
-    private static boolean isCyclic(int n, int[][] edges) {
+    /*private static boolean isCyclic(int n, int[][] edges) {
         List<List<Integer>> adj = new ArrayList<>();
         for (int i = 0; i<n; i++) {
             adj.add(new ArrayList<>());
@@ -41,5 +41,39 @@ public class DetectCycleInDirectedGraph {
         ArrayList<Integer> ans = new ArrayList<>();
         bfs(inDegree, adj, ans);
         return ans.size() != n;
+    }*/
+    private static boolean isCyclic(int n, int[][] edges){
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i<n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]); // directed graph
+        }
+        boolean[] isVisited = new boolean[n];
+        boolean[] path = new boolean[n];
+        for(int i=0; i<n; i++){
+            if(!isVisited[i]){
+                if(dfs(i, isVisited, path, adj)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean dfs(int i, boolean[] isVisited, boolean[] path, List<List<Integer>> adj) {
+        isVisited[i] = true;
+        path[i] = true;
+        for (int ele : adj.get(i)) {
+            if (path[ele]) {
+                return true;
+            }
+            if (!isVisited[ele]) {
+                if(dfs(ele, isVisited, path, adj)) return true;
+            }
+        }
+        path[i] = false; // backtracking
+        return false;
     }
 }
